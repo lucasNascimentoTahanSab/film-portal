43ae369d0ca9125fe127b1f447a728043b38896e
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2022-06-22 00:06:15 -0300

    Correção em PORT.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 
 const app = express()
 
+const PORT = process.env.PORT
+
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
 
-app.listen(8080, () => console.log('Listening on port 8080.'))
+app.listen(PORT, () => console.log('Listening on port 8080.'))
